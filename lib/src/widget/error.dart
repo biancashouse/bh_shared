7219bcdb1a38c2b6a489a3d5b8361499c2d40484
Bianca Cat <git@biancashouse.com>
2024-12-16 00:33:07 +1100

    refactor:improve Error widget

--- lib/src/widget/error.dart
+++ lib/src/widget/error.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class Error extends StatelessWidget {
   final String flutterType;
@@ -7,26 +8,46 @@ class Error extends StatelessWidget {
   final String? errorMsg;
 
   const Error(this.flutterType,
-      {this.color = Colors.red, this.size, this.errorMsg, required super.key});
+      {this.color = Colors.red, this.size, this.errorMsg});
 
   @override
   Widget build(BuildContext context) {
+    double luminance = color.computeLuminance();
+    bool needsDarkBg = luminance > 0.5;
     return Material(
       textStyle: const TextStyle(fontFamily: 'monospace', fontSize: 12),
-      child: SingleChildScrollView(
-        child: Column(children: [
-          Row(
-            children: [
-              Text(flutterType),
-              Icon(Icons.error, color: color, size: size),
-              if (errorMsg != null)
-                Text(
-                  errorMsg!,
-                  style: TextStyle(color: Colors.red),
-                )
-            ],
-          )
-        ]),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(32),
+        child: Container(
+            width: 300,
+            height: 200,
+            color: Colors.red,
+            padding: EdgeInsets.all(30),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(18),
+              child: Container(
+                color: needsDarkBg ? Colors.black : Colors.white,
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(flutterType),
+                        Gap(10),
+                        Icon(Icons.error, color: color, size: size),
+                      ],
+                    ),
+                    if (errorMsg != null)
+                      Text(
+                        errorMsg!,
+                        style: TextStyle(color: Colors.red, fontSize: size),
+                      )
+                  ],
+                ),
+              ),
+            )),
       ),
     );
   }
